0144a8fa0ddf81f5d7dc225c3612e99ab05954d8
gabrielzezze <gabrielzezze@me.com>
2020-11-09 17:30:20 -0300

    Front connection to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Axios from 'axios';
+import { stringify } from 'querystring';
+import React, { useState } from 'react';
+import Classes from './css/App.module.css'
+
+const Api = Axios.create({
+  baseURL: process.env.BACKEND_IP ? process.env.BACKEND_IP : 'localhost:5000',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
 
 function App() {
+
+  const [name, setName] = useState<string>('')
+  const [res, setRes] = useState<{ status: 'ok', message: string | null }>({ status: 'ok', message: null })
+  const onClick = async () => {
+    const response = await Api.post('/transaction', { name })
+    setRes(response.data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={Classes.root}>
+        { 
+          res ? (<h1>{ res.message }</h1>) : null
+        }
+        <form style={{ width: '50%' }}>
+          <h2>Seu Nome para receber uma mensagem inspiradora:</h2>
+          <input placeholder={'Seu nome'} />
+          <button className={Classes.button} onClick={onClick}>Receber inspiracao</button>
+        </form>
     </div>
   );
 }
