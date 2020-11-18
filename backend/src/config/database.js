8c70e988419590bdf5ac2d9038b8a880e92a3caa
gabrielzezze <gabrielzezze@me.com>
2020-11-18 15:54:46 -0300

    DB Password on env Var

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,11 @@
 const ip = process.env.ip ? process.env.ip : process.env.DATABASE_IP
+const password = process.env.DATABASE_PASSWORD ? process.env.DATABASE_PASSWORD : "cloud-password"
 
 if(!ip) console.log('[ERROR] No Ip given')
 
 module.exports = {
   username: "root",
-  password: "cloud-password",
+  password: password,
   database: "Cloud",
   host: ip,
   port: "80",
